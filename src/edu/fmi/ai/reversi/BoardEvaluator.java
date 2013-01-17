d1a69e28f5e34f0fb00a83fcbe1233d7188b6248
martin <asenov.m@gmail.com>
2013-01-18 00:30:12 +0200

    added a tid bit of javadoc

--- src/edu/fmi/ai/reversi/BoardEvaluator.java
+++ src/edu/fmi/ai/reversi/BoardEvaluator.java
@@ -4,7 +4,19 @@ import edu.fmi.ai.reversi.model.Board;
 import edu.fmi.ai.reversi.model.Cell;
 import edu.fmi.ai.reversi.model.Player;
 
+/**
+ * 
+ * A helper class that is used for evaluating the current state of the board.
+ * 
+ * @author martin
+ * 
+ */
 public class BoardEvaluator {
+
+	/**
+	 * {@value}
+	 */
+	private static final int WEIGHT_STABLE_DISCS = 60;
 
 	private final int[][] locationValues;
 
@@ -15,6 +27,17 @@ public class BoardEvaluator {
 				{ -1, 10, 1, 1, 1, 1, 10, -1 }, { 100, -1, 5, 2, 2, 5, -1, 100 } };
 	}
 
+	/**
+	 * Returns the value of the <tt>board</tt> given, based on assessing the
+	 * location of discs of the given <tt>player</tt>
+	 * 
+	 * @param board
+	 *            the board that is to be evaluated
+	 * @param player
+	 *            the player for which the evaluation is to be performed
+	 * @return the ratio between the location-based value of the board for the
+	 *         <tt>player</tt> given and the one for his opponent.
+	 */
 	public float getLocationValue(final Board board, final Player player) {
 		int locationValue = 0;
 		int opponentValue = 0;
@@ -33,10 +56,23 @@ public class BoardEvaluator {
 		return locationValue + opponentValue;
 	}
 
+	/**
+	 * Returns the value of the <tt>board</tt> given, based on assessing the
+	 * stability of the discs that are currently on the board for the
+	 * <tt>player</tt> specified
+	 * 
+	 * @param board
+	 *            the board that is to be evaluated
+	 * @param player
+	 *            the player for whom the board is to be evaluated
+	 * @return the difference between the number of stable discs on the board
+	 *         for the <tt>player</tt> given and his opponent, multiplied by the
+	 *         respective weight
+	 */
 	public int getStabilityValue(final Board board, final Player player) {
 		return player.getSign()
 				* (board.getStableDiscsCount(player) - board.getStableDiscsCount(Player
-						.getOpponent(player))) * 60;
+						.getOpponent(player))) * WEIGHT_STABLE_DISCS;
 	}
 
 }
--- src/edu/fmi/ai/reversi/EventLoop.java
+++ src/edu/fmi/ai/reversi/EventLoop.java
@@ -1,7 +1,18 @@
 package edu.fmi.ai.reversi;
 
+/**
+ * The main class, that is used for starting the application
+ * 
+ * @author martin
+ * 
+ */
 public class EventLoop {
 
+	/**
+	 * Loops until the game finishes with one of the two players winning it.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		final Game game = new Game();
 		while (!game.isFinished()) {
