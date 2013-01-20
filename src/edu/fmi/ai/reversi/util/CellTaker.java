90996aed4e97e25915cd3711e1ce1b39551b4d05
martin <asenov.m@gmail.com>
2013-01-20 19:48:50 +0200

    added some javadoc

--- src/edu/fmi/ai/reversi/util/CellTaker.java
+++ src/edu/fmi/ai/reversi/util/CellTaker.java
@@ -6,7 +6,6 @@ import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-import edu.fmi.ai.reversi.Game;
 import edu.fmi.ai.reversi.model.Board;
 import edu.fmi.ai.reversi.model.Cell;
 import edu.fmi.ai.reversi.model.Player;
@@ -18,9 +17,31 @@ public class CellTaker {
 
 	private final Board board;
 
+	/**
+	 * Creates a new cell taker object to be used for taking cells
+	 * 
+	 * @param checker
+	 *            the move checker that is used when computing the cells for
+	 *            taking
+	 * @param board
+	 *            the board whose cells are to be taken
+	 */
 	public CellTaker(final MoveChecker checker, final Board board) {
 		this.checker = checker;
 		this.board = board;
+	}
+
+	/**
+	 * Takes the cell specified and all the other cells that are affected
+	 * 
+	 * @param cellIndex
+	 *            the board index of the cell that is to be taken
+	 * @param player
+	 *            the player who is taking the cell at the cell index given
+	 * @return the collection of all the affected of the taking cells
+	 */
+	public Collection<Cell> takeCell(final int cellIndex, final Player player) {
+		return takeSurroundedCells(board.get(cellIndex), player);
 	}
 
 	private Collection<Cell> takeSurroundedCells(final Cell cell, final Player player) {
@@ -45,10 +66,6 @@ public class CellTaker {
 		final int startIndex = Math.min(cell.getIndex(), neighbourIndex);
 		final int endIndex = Math.max(cell.getIndex(), neighbourIndex);
 		return takeCells(neighbourIndex, startIndex, endIndex, direction.getIncrement(), player);
-	}
-
-	public Collection<Cell> takeCell(final int cellIndex, final Player player) {
-		return takeSurroundedCells(board.get(cellIndex), player);
 	}
 
 	private Collection<Cell> takeCells(final int fromIndex, final int toIndex, final int step,
